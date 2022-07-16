719a02b0648a5ae2b1f2bfced485f92934db2b09
Eliseo Fuentes Garcia <efuentes@eliseos-mbp.lan>
2022-07-16 17:23:45 -0500

    use port from env

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { sync } = require('./routes');
 var cors = require('cors')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 // const corsOptions ={
 //   origin:'http://localhost:3001', 
 //   credentials:true,            //access-control-allow-credentials:true
